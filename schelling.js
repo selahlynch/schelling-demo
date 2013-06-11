11ad32b2f18f8e905c132ea68178263e8f8c3ae0
Selah Lynch <selah@wharton.upenn.edu>
2013-06-11 11:45:40 -0400

    Successful creation of NxN grid

--- schelling.js
+++ schelling.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
   $("button").click(function(){
     var gridSize = $("#grid-size").val();
-    $("#grid-size-div").html(gridSize);
+    tbl = createTable(gridSize)
+    $("#table-div").text("Putting table here of size " + gridSize + " :)");
+    $("#table-div").append(tbl);
   });
 }); 
+
+
+function createTable(tableSize)
+{
+  var table = $('<table></table>');
+  for(i=0; i<tableSize; i++){
+    var row = $('<tr></tr>');
+    for(j=0; j<tableSize; j++){
+      var cell = $('<td></td>').text(" " + i + j);
+      row.append(cell);
+    }
+    table.append(row);    
+  }
+  return table;
+}
+
+
+
+
+
+
+
+
+
 
 
