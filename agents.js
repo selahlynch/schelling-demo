b8b1f3b4ed57740f5702d4aa2ff5d83e69bdf517
Selah Lynch <selah@wharton.upenn.edu>
2013-06-13 11:50:09 -0400

    fleshed out pseudo code for simplified simulator

--- agents.js
+++ agents.js
@@ -1,35 +1,40 @@
 grid_size = 8;
 num_agents = 5;
 
-///make a set of agents
+//make a grid full of zeros
+grid = [];
+
+//make a set of agents
 dummy_agent = {
   type:0,
   pos_x:0,
   pos_y:0
 }
 
-var agent_array = []
+var agent_array = [];
 
 //initializing the agents
 for (var i=0;i<num_agents;i++)
 {
   agent_array.push(Object.create(dummy_agent));
   agent_array[i].type = 1;
-  //find a random available position
-  //set this agent to that position
+  set_to_random_available_pos(agent_array[i], grid);
 }
 
-//agents are initialized
-
-//increment the agents
-
-function increment_agents(agent_array){
+function increment_agents(agent_array, grid){
   //for each agent
-  //decide if it wants to move
-  //find a random available position
-  //update it to this position
+    //decide if it wants to move
+    set_to_random_available_pos(agent, grid);
 }
 
+function set_to_random_available_pos(agent, grid){
+  //find a random available position
+    //obtain a set of available positions on grid
+    //choose one at random
+  //set this agent to that position
+    //update the grid
+    //update the xy coords of the agent
+}
 
 
 
