383324f38999f4f90044361b41879a72cb100a69
Selah Lynch <selah@wharton.upenn.edu>
2013-06-14 11:18:12 -0400

    setup agent handling code independently from rendering

--- agents.js
+++ agents.js
@@ -1,8 +1,9 @@
-grid_size = 8;
-num_agents = 5;
+grid_size = 3;
+num_agents = 4;
+neighbor_threshold = 1;
 
 //make a grid full of zeros
-grid = [];
+grid = zeros([grid_size, grid_size]);
 
 //make a set of agents
 dummy_agent = {
@@ -22,36 +23,61 @@ for (var i=0;i<num_agents;i++)
 }
 
 function increment_agents(agent_array, grid){
-  //for each agent
-    //decide if it wants to move
-    set_to_random_available_pos(agent, grid);
+  for(var i = 0; i < agent_array.length; i++){
+    var agent = agent_array[i];
+    if (num_neighbors(agent, grid) > neighbor_threshold){
+      set_to_random_available_pos(agent, grid);    
+    }
+  }
 }
+
+function num_neighbors(agent, grid){
+  var x = agent.pos_x;
+  var y = agent.pos_y;
+  var neighbors = 0;
+  if (x-1 > 0 && grid[x-1][y-1]) neighbors++;
+  if (x-1 > 0 && grid[x-1][y]) neighbors++;
+  if (x-1 > 0 && grid[x-1][y+1]) neighbors++;
+  if (grid[x][y-1]) neighbors++;
+  if (grid[x][y+1]) neighbors++;
+  if (x+1 < grid.length-1 && grid[x+1][y-1]) neighbors++;
+  if (x+1 < grid.length-1 && grid[x+1][y]) neighbors++;
+  if (x+1 < grid.length-1 && grid[x+1][y+1]) neighbors++;
+  return neighbors;
+}
+
 
 function set_to_random_available_pos(agent, grid){
-  //find a random available position
-    //obtain a set of available positions on grid
-    //choose one at random
-  //set this agent to that position
-    //update the grid
-    //update the xy coords of the agent
+  avail_pos_array = avail_pos(grid);
+  var new_pos = avail_pos_array[Math.floor(Math.random()*avail_pos_array.length)];
+  var old_pos = [agent.pos_x, agent.pos_y];
+  grid[old_pos[0]][old_pos[1]] = 0;
+  grid[new_pos[0]][new_pos[1]] = agent;
+  agent.pos_x = new_pos[0];
+  agent.pos_y = new_pos[1];
+}
+
+function avail_pos(grid){
+  avail_pos_array = [];
+  for(var i = 0; i<grid.length; i++){
+    for(var j = 0; j<grid.length; j++){
+      if(!grid[i][j]){
+        avail_pos_array.push([i,j]);
+      }
+    }  
+  }
+  return avail_pos_array;
 }
 
 
+function zeros(dimensions) {
+    var array = [];
 
+    for (var i = 0; i < dimensions[0]; ++i) {
+        array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return array;
+}
 
 
