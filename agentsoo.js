1737398cb04d36b5bd541211ce5a25d671e01d2c
Selah Lynch <selah@wharton.upenn.edu>
2013-06-18 10:53:28 -0400

    finished up a working version of sim object

--- agentsoo.js
+++ agentsoo.js
@@ -1,8 +1,14 @@
-//build starter object using closure
+//is this object too big??
+
 var sim = function(){
 
   var grid_size, agent_count, threshold;
   var agent_grid, agent_array;
+
+  var available_position = function(){
+    avpos = available_positions();
+    return avpos[Math.floor(Math.random()*avpos.length)];
+  };
 
   var available_positions = function(){
     var res = [];
@@ -16,7 +22,29 @@ var sim = function(){
     return res;
   };
 
-  var is_happy = function(){};
+  var is_happy = function(agent){
+    var x = agent.pos_x;
+    var y = agent.pos_y;
+    var neighbor_count = 0;
+    if (x-1 > 0 && agent_grid[x-1][y-1]) neighbor_count++;
+    if (x-1 > 0 && agent_grid[x-1][y]) neighbor_count++;
+    if (x-1 > 0 && agent_grid[x-1][y+1]) neighbor_count++;
+    if (agent_grid[x][y-1]) neighbor_count++;
+    if (agent_grid[x][y+1]) neighbor_count++;
+    if (x+1 < agent_grid.length-1 && agent_grid[x+1][y-1]) neighbor_count++;
+    if (x+1 < agent_grid.length-1 && agent_grid[x+1][y]) neighbor_count++;
+    if (x+1 < agent_grid.length-1 && agent_grid[x+1][y+1]) neighbor_count++;
+    return neighbor_count < threshold;    
+  };
+  
+  var move_randomly = function(agent){
+    old_pos = [agent.pos_x, agent.pos_y]
+    new_pos = available_position();
+    agent.pos_x = new_pos[0];
+    agent.pos_y = new_pos[1];
+    agent_grid[new_pos[0]][new_pos[1]] = agent;
+    agent_grid[old_pos[0]][old_pos[1]] = 0;
+  };
 
   var place_new_agent = function(pos){
     agent = {
@@ -29,37 +57,43 @@ var sim = function(){
   };
 
   return {
-    init: function(gs, ac, th){
+    init: function(gs, ac, th){ //maybe this isn't the best style??
       grid_size = gs;
       agent_count = ac; 
       threshold = th;
       agent_grid = zeros([grid_size, grid_size]);
       agent_array = [];
       for(var i = 0; i < agent_count; i++){
-        avpos = available_positions();
-        new_pos = avpos[Math.floor(Math.random()*avpos.length)];
-        place_new_agent(new_pos);
+        place_new_agent(available_position());
       }
     },
 
     update: function(){
+      for(var i = 0; i < agent_count; i++){
+        if(!is_happy(agent_array[i])){
+          move_randomly(agent_array[i]);
+        }
+      }
     },
 
     bitmap: function(){
-      return agent_grid;
+      var x,y;
+      var bitmap = zeros([grid_size, grid_size]);
+      for(var i = 0; i < agent_count; i++){
+        x = agent_array[i].pos_x;
+        y = agent_array[i].pos_y;
+        bitmap[x][y] = agent_array[i].state;
+      }
+      return bitmap;
     }
 
   };
 }();
 
 
-
-
-
-
-
-
 ////functions external to this object/////
+//should I be putting this in the object???
+//this is an external dependency!
 
 function zeros(dimensions) {
     var array = [];
