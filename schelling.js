6610f1f6d2f3df0e70c5c7a0e1f6a2bc0fecaf29
Selah Lynch <selah@wharton.upenn.edu>
2013-06-20 09:44:00 -0400

    cleaned up schelling.js, put things in functions

--- schelling.js
+++ schelling.js
@@ -1,37 +1,35 @@
 $(document).ready(function(){
-
-  var user_grid_size = $("#grid-size-inp").val();
-  var user_num_agents = $("#num-agents-inp").val();
-  var user_agent_threshold = $("#agent-threshold-inp").val();
-
-  sim.init(user_grid_size, user_num_agents, user_agent_threshold);
-  var tbl = render_grid(sim.bitmap());
-  $("#table-div").html(tbl);  
-
-
+  initialize();
   $("#init-button").click(function(){
-    var user_grid_size = $("#grid-size-inp").val();
-    var user_num_agents = $("#num-agents-inp").val();
-    var user_agent_threshold = $("#agent-threshold-inp").val();
-
-    sim.init(user_grid_size, user_num_agents, user_agent_threshold);
-    var tbl = render_grid(sim.bitmap());
-    $("#table-div").html(tbl);  
+    initialize();
   });
 
   $("#update-button").click(function(){
-    sim.update();
-    var tbl = render_grid(sim.bitmap());
-    $("#table-div").html(tbl);  
+    update();
   });  
 
 }); 
 
 
+var update = function(){
+  sim.update();
+  print_table();
+};
+
+var initialize = function(){
+  var user_grid_size = $("#grid-size-inp").val();
+  var user_num_agents = $("#num-agents-inp").val();
+  var user_agent_threshold = $("#agent-threshold-inp").val();
+
+  sim.init(user_grid_size, user_num_agents, user_agent_threshold);
+  print_table();
+};
 
 
-
-
+var print_table = function(){
+  var tbl = render_grid(sim.bitmap());
+  $("#table-div").html(tbl);    
+};
 
 
 
